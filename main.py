d3594a4a8f1c0b8a24b42f6e6e8d3c57e4ddc18a
TahaThinks <tahaa4587@gmail.com>
2024-01-28 15:08:26 +0400

    If user input is report then print the report, else check if the drink is available

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 available_drinks = Menu()
-requested_drink = input(f"What would you like {available_drinks.get_items()}?")
+machine_stats = CoffeeMaker()
+requested_drink = input(f"What would you like {available_drinks.get_items()}? ").lower()
+if requested_drink == 'report':
+    machine_stats.report()
+else:
+    available_drinks.find_drink(requested_drink)
