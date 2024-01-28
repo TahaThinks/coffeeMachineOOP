cf3b50c54815df348383c5c29ec1e2cddb8f7445
TahaThinks <tahaa4587@gmail.com>
2024-01-28 16:04:34 +0400

    Print Report

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-available_drinks = Menu()
-machine_stats = CoffeeMaker()
-requested_drink = input(f"What would you like {available_drinks.get_items()}? ").lower()
-if requested_drink == 'report':
-    machine_stats.report()
-else:
-    available_drinks.find_drink(requested_drink)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+print(money_machine.report())
+print(coffee_maker.report())
+
